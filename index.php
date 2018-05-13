60450b6928e564aa60205b0dbb87fd061f507f63
Cas Eliens <cas.eliens@gmail.com>
2018-05-13 22:47:47 +0200

    do the thing

--- index.php
+++ index.php
@@ -17,5 +17,40 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.                          *
  **************************************************************************************************/
 
+$year = (isset($_GET['year'])) ? $_GET['year'] : date("Y");
+$week = (isset($_GET['week']) && $_GET['week'] <= 53) ? $_GET['week'] : date("W");
+
+$url = "https://www.top40.nl/tipparade/$year/week-$week";
+
+$html = file_get_contents($url);
+
+$dom = new DOMDocument;
+$dom->loadHTML($html);
+$finder = new DomXPath($dom);
+$classname = "new";
+$nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+
+$results = "NEW SONGS FOR WEEK $week $year";
+
+foreach ($nodes as $node) {
+    $text = $node->textContent;
+
+    $lines = explode("\n", $text);
+    $data = [];
+
+    // line 0   date
+    // line 1   title
+    // line 2   artist
+
+    for ($i = 1; $i <3; $i++) {
+        $line = $lines[$i];
+        $parsed = preg_replace("/\s{2,}/", " ", $line);
+        $data[$i - 1] = $parsed;
+    }
+
+    $results = $results . "\n$data[0] BY $data[1]";
 
 
+}
+
+echo $results;
